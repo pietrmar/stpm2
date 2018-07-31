d6b593337bdb0ed031d8903b92d08610b47d5b60
Martin Pietryka <martin@pietryka.at>
2018-07-31 22:43:53 +0200

    examples: implement stpm2_get_random() example
    Signed-off-by: Martin Pietryka <martin@pietryka.at>

--- examples/get_random.c
+++ examples/get_random.c
@@ -4,11 +4,27 @@
 
 int main(int argc, char *argv[])
 {
-	uint8_t random_byte;
+	stpm2_context ctx;
+	int ret;
+	uint8_t random_bytes[16];
 
-	stpm2_get_random(NULL, &random_byte, 1);
+	ret = stpm2_init(&ctx);
+	if (ret < 0) {
+		printf("stpm2_init() failed\n");
+		return 1;
+	}
 
-	printf("0x%02x\n", random_byte);
+	ret = stpm2_get_random(&ctx, random_bytes, 16);
+	if (ret < 0) {
+		printf("stpm2_get_random() failed\n");
+		return 1;
+	}
+
+	for (int i = 0; i < 16; i++) {
+		printf("0x%02x ", random_bytes[i]);
+	}
+
+	printf("\n");
 
 	return 0;
 }
